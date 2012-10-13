1550ef0f53b34eaa922c8e883c7dbf6672cf6b65
ktf <not.discostoo@gmail.com>
2012-10-12 23:02:51 -0700

    properly split words with new dictionary

--- haikubot.py
+++ haikubot.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 
 
-import hyphen
+import linecache
+import random
+import re
 import sys
 
 
-WORD_LIST = 'american-english'
+WORD_LIST = 'hyph-dict'
+LIST_LENGTH = 187175 # we're cheating here -- we know the length of the list
 
 
 def get_syllables(word):
-    """Return the syllables of a word, as a list. Because
-    Hyphenator.syllables() returns [] for words of length <= 3, we have to
-    check for the empty list before returning."""
-    hyph = hyphen.Hyphenator('en_US')
-    syllables = hyph.syllables(unicode(word))
-    if not syllables:
-        syllables.append(unicode(word))
-    return syllables
+    """Return a list of syllables in the given word."""
+    split_list = re.split('\W', word)
+    # the weird ASCII character causes empty strings which we need to remove:
+    return filter(None, split_list)
 
 
 def get_num_syllables(word):
@@ -27,18 +26,19 @@ def get_num_syllables(word):
 def get_random_word(word_list):
     """Return a random word from the given word list file. Assumes one word per
     line."""
-    return "blah"
+    return linecache.getline(word_list, random.randint(1, LIST_LENGTH)).strip()
 
 
 def main():
     """My main() man."""
-    word = sys.argv[1]
-    print get_syllables(word)
-    print get_num_syllables(word)
+#    word = sys.argv[1]
+#    print get_syllables(word)
+#    print get_num_syllables(word)
 
-    with open(WORD_LIST) as word_list:
-        rand_word = get_random_word(word_list)
+    rand_word = get_random_word(WORD_LIST)
     print rand_word
+    print get_syllables(rand_word)
+    print get_num_syllables(rand_word)
 
 
 if __name__ == '__main__':
