cb7d4005b5552432d03de062c5321fc56b4600cf
ktf <not.discostoo@gmail.com>
2012-10-13 22:45:11 -0700

    generate haiku!

--- haikubot.py
+++ haikubot.py
@@ -23,22 +23,36 @@ def get_num_syllables(word):
     return len(get_syllables(word))
 
 
-def get_random_word(word_list):
+def get_random_word(word_list, list_length):
     """Return a random word from the given word list file. Assumes one word per
     line."""
-    return linecache.getline(word_list, random.randint(1, LIST_LENGTH)).strip()
+    return linecache.getline(word_list, random.randint(1, list_length)).strip()
+
+
+def clean_word(word):
+    """Our handy dictionary uses ASCII 165 to separate syllables -- this
+    function removes them for the output of our... poetry."""
+    return filter(lambda x: x != chr(165), word)
+
+
+def get_haiku_line(syllables):
+    """Return a line of haiku with the specified number of syllables."""
+    remaining_syllables = syllables
+    line = ''
+    while(remaining_syllables > 0):
+        word = get_random_word(WORD_LIST, LIST_LENGTH)
+        syl = get_num_syllables(word)
+        if syl <= remaining_syllables: # if the word's too long, ruh roh
+            remaining_syllables -= syl
+            line += clean_word(word) + ' '
+    return line.strip() # kill remaining whitespace
 
 
 def main():
     """My main() man."""
-#    word = sys.argv[1]
-#    print get_syllables(word)
-#    print get_num_syllables(word)
-
-    rand_word = get_random_word(WORD_LIST)
-    print rand_word
-    print get_syllables(rand_word)
-    print get_num_syllables(rand_word)
+    print get_haiku_line(5)
+    print get_haiku_line(7)
+    print get_haiku_line(5)
 
 
 if __name__ == '__main__':
