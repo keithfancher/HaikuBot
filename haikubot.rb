8346521d99c71480ec3d6729a7ddf2e624f190a6
ktf <not.discostoo@gmail.com>
2013-04-06 12:37:13 -0700

    bump copyright year

--- haikubot.rb
+++ haikubot.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# Copyright 2012 Keith Fancher
+# Copyright 2012,2013 Keith Fancher
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- test_haikubot.rb
+++ test_haikubot.rb
@@ -1,4 +1,4 @@
-# Copyright 2012 Keith Fancher
+# Copyright 2012,2013 Keith Fancher
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
